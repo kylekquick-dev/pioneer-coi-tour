3fb7471d9e18a85ff2fcf990f6c61ede5dd49d38
kylekquick-dev <kylekquick@gmail.com>
2026-08-17 08:16:10 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
 
+export const metadata: Metadata = {
+  title: "Pioneer Century of Innovation Crop Tour",
+  description: "A mobile virtual crop tour for Pioneer Seeds."
+};
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
